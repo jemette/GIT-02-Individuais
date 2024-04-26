bfb4099760ca9e1b22903bc77fd3c6fb5932079c
Julia E. Mette <juliaemanuele.mette@philips.com>
2024-04-26 11:19:01 -0300

    [SO-123] fix(XPTO) fixing bug

--- exercicioDois.java
+++ exercicioDois.java
@@ -1,10 +1,18 @@
 package aula;
 
+import java.util.Scanner;
+
+
 public class exercicioDois {
 	  public static void main (String [] args) {
+		  Scanner scan = new Scanner(System.in);
+			String name;
 		
 			System.out.println("Exercício 02 Online");
+			System.out.println("Escreva seu nome: ");
+			name = scan.next();
 
+			System.out.println(name);
 		
 		}
 }
